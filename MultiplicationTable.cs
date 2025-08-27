793289a1acd6498762e80df32335554b38d846c5
Rheetham Menon <rheethammenon@gmail.com>
2025-08-27 12:14:15 +0530

    Multiplication table using error handling for parsing

---
+++ MultiplicationTable.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MyFIrstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number for the table: ");
+            
+            if(int.TryParse(Console.ReadLine(), out int num))
+            {
+                for (int i = 1; i <= 10; ++i)
+                {
+                    Console.WriteLine(num + " * " +  i + " = " + num * i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
+        }
+    }
+}
